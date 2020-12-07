f76eb7225eff6f439085871fdbe5ae5d381c05e8
Mirco Franzek <mirco.franzek@otto.de>
2020-12-07 15:49:19 +0100

    Tests for field -> name code

--- webserver/reflection_test.go
+++ webserver/reflection_test.go
@@ -7,17 +7,40 @@ import (
 )
 
 func TestFieldsAndNames(t *testing.T) {
-	// given
-	type TestStruct struct {
-		Name  string
-		Int   int64
-		Float float64
-		Bool  bool
-	}
 
-	// when
-	names := FieldsAndNames(TestStruct{})
+	t.Run("struct in struct", func(t *testing.T) {
+		type Embedded struct {
+			Name string
+		}
 
-	// then
-	assert.Equal(t, reflect.String, names["Name"])
+		type NestedStruct struct {
+			Foo Embedded
+		}
+
+		// when
+		names := FieldsAndNames(NestedStruct{})
+
+		// then
+		assert.Equal(t, reflect.Struct, names["Foo"])
+	})
+
+	t.Run("multiple fields", func(t *testing.T) {
+		// given
+		type TestStruct struct {
+			Name  string
+			Int   int64
+			Float float64
+			Bool  bool
+		}
+
+		// when
+		names := FieldsAndNames(TestStruct{})
+
+		// then
+		assert.Equal(t, reflect.String, names["Name"])
+		assert.Equal(t, reflect.Int64, names["Int"])
+		assert.Equal(t, reflect.Float64, names["Float"])
+		assert.Equal(t, reflect.Bool, names["Bool"])
+	})
+
 }
