f86dc88ba3b58f1e706d492c2284d312ae9ccbef
Mirco Franzek <mirco.franzek@otto.de>
2020-12-08 11:39:44 +0100

    Moved go.mod to the root

--- randomize/internal/randomizer_test.go
+++ randomize/internal/randomizer_test.go
@@ -1,11 +1,17 @@
 package internal
 
-import "testing"
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
 
 func TestRandomizeSimpleValues(t *testing.T) {
 
 	t.Run("randomize string", func(t *testing.T) {
-
+		type StringStruct struct {
+			String string
+		}
+		got := randomize(StringStruct{})
 	})
 
 	t.Run("randomize integer", func(t *testing.T) {
